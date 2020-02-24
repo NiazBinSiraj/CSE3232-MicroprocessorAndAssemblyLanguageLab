b4a95c9d2df0026f4798c0b59841b7004e642a1a
Niaz-Bin-Siraj <niaz9767@gmail.com>
2020-02-25 01:22:39 +0600

    vowel counter corrected

--- 3_a_CountingVowelConsonentDigitSpace.asm
+++ 3_a_CountingVowelConsonentDigitSpace.asm
@@ -3,10 +3,6 @@ INCLUDE "EMU8086.INC"
 .STACK 100H
 
 .DATA
-    vowel DB 'A','E','I','O','U','a','e','i','o','u'
-    consonant DB 'B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','V','W','X','Y','Z','b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'
-    digit DB '0','1','2','3','4','5','6','7','8','9'
-    
     vc DB ?
     cc DB ?
     dc DB ?
@@ -34,22 +30,66 @@ INCLUDE "EMU8086.INC"
             CMP AL,13
             JZ ENDINPUT
             
-            CALL CHECK_VOWEL
-            CMP mark,'v'
+            CMP AL, 32
+            JE IS_SPACE
+            
+            CMP AL, 'A'
             JE IS_VOWEL
             
-            CALL CHECK_CONSONANT
-            CMP mark,'c'
-            JE IS_CONSONANT
+            CMP AL, 'E'
+            JE IS_VOWEL
             
-            CALL CHECK_DIGIT
-            CMP mark,'d'
-            JE IS_DIGIT
+            CMP AL, 'I'
+            JE IS_VOWEL
             
-            CALL CHECK_SPACE
-            CMP mark,'s'
-            JE IS_SPACE           
+            CMP AL, 'O'
+            JE IS_VOWEL
             
+            CMP AL, 'U'
+            JE IS_VOWEL
+            
+            CMP AL, 'a'
+            JE IS_VOWEL
+            
+            CMP AL, 'e'
+            JE IS_VOWEL
+            
+            CMP AL, 'i'
+            JE IS_VOWEL
+            
+            CMP AL, 'o'
+            JE IS_VOWEL
+            
+            CMP AL, 'u'
+            JE IS_VOWEL
+            
+            
+            CMP AL, '0'
+            JL  CHECK_CAPITAL
+            
+            CMP AL, '9'
+            JG  CHECK_CAPITAL
+            
+            JMP IS_DIGIT
+            
+            CHECK_CAPITAL:
+                CMP AL, 'A'
+                JL INPUT
+                
+                CMP AL, 'Z'
+                JG CHECK_SMALL
+                
+                JMP IS_CONSONANT
+                
+            CHECK_SMALL:
+                CMP AL, 'a'
+                JL INPUT
+                
+                CMP AL, 'z'
+                JG INPUT
+                
+                JMP IS_CONSONANT
+                                            
             JMP INPUT
             
             IS_VOWEL:
@@ -73,6 +113,11 @@ INCLUDE "EMU8086.INC"
             CALL NEWLINE
             
             
+        ADD vc,48
+        ADD cc,48
+        ADD dc,48
+        ADD sc,48
+        
         PRINT "Vowel : "        
         MOV AH,2
         MOV DL,vc
@@ -109,76 +154,5 @@ INCLUDE "EMU8086.INC"
         INT 21H
         RET
     NEWLINE ENDP
-    
-    CHECK_VOWEL PROC
-        MOV size,10
-        MOV SI,0
-        FOR_VOWEL:
-            CMP AL, vowel[SI]
-            JE GOT_VOWEL
-            
-            DEC size
-            CMP size,0
-            JE ENDFOR_VOWEL
-                       
-            INC SI
-            JMP FOR_VOWEL
-        ENDFOR_VOWEL:    
-            RET
-        GOT_VOWEL:
-            MOV mark, 'v'
-            RET
-    CHECK_VOWEL ENDP
-    
-    CHECK_CONSONANT PROC
-        MOV size,42
-        MOV SI,0
-        FOR_CONSONANT:
-            CMP AL, consonant[SI]
-            JE GOT_CONSONANT
-            
-            DEC size
-            CMP size,0
-            JE ENDFOR_CONSONANT
-                       
-            INC SI
-            JMP FOR_CONSONANT
-        ENDFOR_CONSONANT:    
-            RET
-        GOT_CONSONANT:
-            MOV mark, 'c'
-            RET
-    CHECK_CONSONANT ENDP
-    
-    CHECK_DIGIT PROC
-        MOV size,10
-        MOV SI,0
-        FOR_DIGIT:
-            CMP AL, digit[SI]
-            JE GOT_DIGIT
-            
-            DEC size
-            CMP size,0
-            JE ENDFOR_DIGIT
-                       
-            INC SI
-            JMP FOR_DIGIT
-        ENDFOR_DIGIT:    
-            RET
-        GOT_DIGIT:
-            MOV mark, 'd'
-            RET
-    CHECK_DIGIT ENDP
-    
-    CHECK_SPACE PROC
-        CMP AL, 20H
-        JE GOT_SPACE
-        
-        RET
-        
-        GOT_SPACE:
-            MOV mark, 's'
-            RET
-    CHECK_SPACE ENDP
     
 END MAIN
